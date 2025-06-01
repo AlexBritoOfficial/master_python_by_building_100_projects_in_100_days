c517b5e81714b1c5f365b9f88e743380002ffa24
Alex Brito <alexbrito@Alexs-MacBook-Pro.local>
2025-06-01 17:46:13 -0400

    Working on Step 5 part c

--- Section_15_Day_15_Intermediate_Local_Development_Environment_Setup_and_the_Coffee_Machine/main.py
+++ Section_15_Day_15_Intermediate_Local_Development_Environment_Setup_and_the_Coffee_Machine/main.py
@@ -1,3 +1,7 @@
+""""
+Given the course outline of this Udemy course, the course has not yet reached a point where the instructor speaks on OOP, hence the reason no OOP
+"""
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -33,13 +37,33 @@ resources = {
 
 if __name__ == "__main__":
 
-    isDrinkDispensed = False
-    enoughResources = True
-    isMoneyHanded = False
-    isResourcesEnough = True
-    userTotal = 0
+    # This flag is responsible for letting us know, if the drink was dispensed.
+    isDrinkDispensed: bool = False
 
-    while (not isDrinkDispensed):
+    # This flag is responsible for letting us know, if the drink was dispensed.
+    isResourcesEnough: bool = True
+
+    # This flag is responsible for letting us know, if the user has fully inserted their change.
+    isMoneyHanded: bool = False
+
+    userTotal: float = 0
+
+    userTotalDict: dict = {
+        "quarter": {
+            "count": 0
+        },
+        "dime": {
+            "count": 0
+        },
+        "nickel": {
+            "count": 0
+        },
+        "penny": {
+            "count": 0
+        }
+    }
+
+    while not isDrinkDispensed:
         userResponse = input("Would you like order a espresso/latte/cappuccino ? \n")
 
         if userResponse.lower().strip() == "no":
@@ -66,25 +90,55 @@ if __name__ == "__main__":
 
         else:
 
-            orderIngredients = MENU[userResponse]["ingredients"]
-            resourceKeysList = list(resources.keys())
-
-            for i, (key, value) in enumerate(orderIngredients.items()):
+            """
+            Process coins.
+                
+                a.  If there are sufficient resources to make the drink selected, then the program should
+                    prompt the user to insert coins.
+                
+                b.  Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+                
+                c.  Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
+                    pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+            """
+            for i, (key, value) in enumerate(MENU[userResponse]["ingredients"].items()):
 
                 if value <= resources[list(resources.keys())[i]]:
                     continue
                 else:
                     isResourcesEnough = False
 
-            print(f"The total for your {userResponse}, comes out to ${MENU[userResponse]["cost"]}")
+            print(f"\nThe total for your {userResponse}, comes out to ${MENU[userResponse]["cost"]}\n")
 
-            while userTotal <=  MENU[userResponse]["cost"] and isResourcesEnough:
+            while not isMoneyHanded:
 
                 userMoney = input(
-                    "Insert quarters, dimes, nickels, and pennies into machine \n")
+                    "Insert quarters, dimes, nickels, and pennies into machine \n"
+                    "\nPlease enter DONE, when you are done inserting your coins.\n")
 
-                if  userMoney == "" or float(userMoney) not in [0.25, 0.10, .05, .01]:
+                if userMoney == "DONE".lower() or userMoney == "DONE".upper():
+                    isMoneyHanded = True
+
+                elif userMoney == "" or float(userMoney) not in [0.25, 0.10, .05, .01]:
                     print("Please only insert quarters, dimes, nickles, and pennies")
+
+
                 else:
-                    userTotal += float(userMoney)
-                    print(f"User Total: {userTotal}")
+
+                    if float(userMoney) == .25:
+                        userTotalDict["quarter"]["count"] = userTotalDict["quarter"]["count"] + 1
+
+                    if float(userMoney) == .10:
+                        userTotalDict["dime"]["count"] = userTotalDict["dime"]["count"] + 1
+
+                    if float(userMoney) == .05:
+                        userTotalDict["nickel"]["count"] = userTotalDict["nickel"]["count"] + 1
+
+                    if float(userMoney) == .01:
+                        userTotalDict["penny"]["count"] = userTotalDict["penny"]["count"] + 1
+
+            """
+                    Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
+                    pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+            """
+            print(userTotalDict)
