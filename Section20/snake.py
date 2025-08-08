c31fdf86a254a8b9491b6a0c5fc5c5379409e18c
Alex Brito <alexbrito@Alexs-MacBook-Pro.local>
2025-08-07 20:23:08 -0400

    Finished Section 20

---
+++ Section20/snake.py
@@ -0,0 +1,49 @@
+from turtle import Turtle
+
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    segment_list = []
+    start_x = 0
+    step = -20
+
+    def __init__(self):
+        self.create_snake()
+        self.head = self.segment_list[0]
+
+    def create_snake(self):
+        for i in range(3):
+            turtle = Turtle("square")
+            turtle.color("white")
+            turtle.penup()
+            turtle.goto(self.start_x + (self.step * i), 0)
+            self.segment_list.append(turtle)
+
+
+    def move(self):
+        for segment_num in range(len(self.segment_list) - 1, 0, -1):
+            new_x = self.segment_list[segment_num - 1].xcor()
+            new_y = self.segment_list[segment_num - 1].ycor()
+            self.segment_list[segment_num].goto(new_x, new_y)
+        self.segment_list[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
